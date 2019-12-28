a913cbca67a4a4d0238834c1934eec217eb2b556
League Student <league@iMac-24.attlocal.net>
2019-12-28 11:25:53 -0800

    started level 1

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -1,7 +1,23 @@
 package _01_houses;
 
+import org.jointheleague.graphical.robot.Robot;
+
 public class Houses {
 	public void run() {
-		
+		Robot rob = new Robot();
+		rob.miniaturize();
+		rob.setSpeed(500);
+		rob.setPenColor(0,255,0);
+		rob.setPenWidth(10);
+		rob.penDown();
+		rob.setX(800);
+		rob.setY(400);
+		rob.turn(-90);
+		rob.move(50);
+		rob.turn(90);
+		rob.setPenColor(150,255,100);
+		rob.move(200);
+		rob.turn(-90);
+		rob.move(50);
 	}
 }
