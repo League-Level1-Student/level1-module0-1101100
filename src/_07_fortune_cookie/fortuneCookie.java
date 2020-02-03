a5b958cdc1f3d504b777f74d45e8ecbf1d681e8d
1101100 <joshtang255@gmail.com>
2020-02-02 19:13:32 -0800

    finished module

---
+++ src/_07_fortune_cookie/fortuneCookie.java
@@ -0,0 +1,55 @@
+package _07_fortune_cookie;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class fortuneCookie implements ActionListener{
+	JFrame frame = new JFrame();
+    JButton button = new JButton();
+ 
+    public void showButton() {
+    	  System.out.println("Button clicked");
+    	    frame.setVisible(true);
+
+    	    frame.add(button);
+    	    frame.pack();
+    	    button.addActionListener(this);
+    		
+    	    button.setText("click here for fortune");
+    	    	
+    	    	frame.pack(); 
+
+   }
+
+	
+	public void actionPerformed(ActionEvent e) {
+		JOptionPane.showMessageDialog(null, "woohoo");
+		Random rand = new Random();
+		int ran = rand.nextInt(5);
+		if (ran == 0) {
+			JOptionPane.showMessageDialog(null, "[fortune0]");
+		}
+		else if (ran == 1) {
+			JOptionPane.showMessageDialog(null, "[fortune1]");
+		}
+		else if (ran == 2) {
+			JOptionPane.showMessageDialog(null, "[fortune2]");
+		}
+		else if (ran == 3) {
+			JOptionPane.showMessageDialog(null, "[fortune3]");
+		}
+		else if (ran == 4) {
+			JOptionPane.showMessageDialog(null, "[fortune4]");
+		}
+
+		
+		
+	}
+
+
+}
